f1eeff64ed49f281d25990ee106d26c484337c1c
Jakub Staňo <ajax770sk@gmail.com>
2022-04-12 00:13:10 +0200

    Fix ShippingMethods

--- src/Model/PaymentMethodRestrictionTrait.php
+++ src/Model/PaymentMethodRestrictionTrait.php
@@ -19,7 +19,7 @@ trait PaymentMethodRestrictionTrait
 
     /**
      * @var Collection<array-key, ShippingMethodInterface>|ShippingMethodInterface[]
-     * @ORM\ManyToMany(targetEntity="Sylius\Component\Core\Model\ShippingMethod", inversedBy="paymentMethods")
+     * @ORM\ManyToMany(targetEntity="Sylius\Component\Core\Model\ShippingMethodInterface", inversedBy="paymentMethods")
      * @ORM\JoinTable(name="threebrs_payment_method_shipping_method",
      *     joinColumns={@ORM\JoinColumn(name="payment_method_id", referencedColumnName="id")},
      *     inverseJoinColumns={@ORM\JoinColumn(name="method_shipping_id", referencedColumnName="id")}
